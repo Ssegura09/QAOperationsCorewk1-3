0a5e3ad7cef725b65d39627b15e4f505bcccbe05
Stephanie Segura <66101450+Ssegura09@users.noreply.github.com>
2021-05-15 14:37:35 -0500

    Initial Commit

---
+++ Starter-HelloWorld/src/com/qa/startworld/Example.java
@@ -0,0 +1,16 @@
+package com.qa.startworld;
+
+public class Example {
+
+	public static void main(String[] args) {
+		String obi = "Hello There";
+		System.out.println(obi);
+		calculator();
+	}
+	
+	public static void calculator() {
+		System.out.println("General Kenobbbiiii");
+		
+	}
+
+}
