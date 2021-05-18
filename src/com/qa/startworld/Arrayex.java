2ba09cd450a3b0903ab851d98f4596f5b3db9619
Ssegura09 <stephaniemsegura09@gmail.com>
2021-05-18 11:35:40 -0500

    SS: string manipulation exercise done

--- src/com/qa/startworld/Arrayex.java
+++ src/com/qa/startworld/Arrayex.java
@@ -1,6 +1,7 @@
 package com.qa.startworld;
 
 import java.util.Arrays;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
@@ -14,21 +15,38 @@ public class Arrayex {
 		matrix();
 		sandwiches();
 		favoriteClass();
+		method();
 
 	}
 
 	
 	public static void matrix() {
+	
+		int[] arr = new int[10];
+		for (int i = 1; i <= arr.length; i++) {
+			for (int j = 1; j <= arr.length; j++) {
+//				System.out.println(i + "*" + j + "=" + (i * j));
+			}
+		}
+	}
+	
+	public static void method() {
 //		Create a for loop that populates an integer array with values, outputting them at each iteration.
 //		Then create another loop that iterates through the array, changing the values at each point to equal
 //		itself times 10, outputting them at each iteration.
 //		
 		int[] arr = new int[10];
-		for (int i = 1; i <= arr.length; i++) {
-			for (int j = 1; j <= arr.length; j++) {
-				System.out.println(i + "*" + j + "=" + (i * j));
-			}
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+//			System.out.println(arr[i] + " ");
 		}
+		
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = arr[i] * 10;
+//			System.out.println(arr[i] + " ");
+		}
+		
+		
 	}
 	
 	public static void sandwiches() {
@@ -74,7 +92,7 @@ public class Arrayex {
 		
 		//for each loop in Java
 		for (int i : faveNums) {
-			System.out.println(i);
+//			System.out.println(i);
 		}
 	}
 
